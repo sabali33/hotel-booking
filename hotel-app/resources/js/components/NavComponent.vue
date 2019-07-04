17396fa61c91eb41bda5ac7bee1f010c388d7c4d
Eliasu Abraman <sabalisfi@yahoo.com>
2019-07-04 14:55:41 +0000

    Move admin nav to a vue component

---
+++ hotel-app/resources/js/components/NavComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="navigation py-5">
+                
+        <ul class="nav-list list-group list-unstyled ">
+            <li class="nav-item list-group-item">
+                <a href="/settings">Hotel Details</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/rooms">Rooms</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/room-types">Room Types</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/room-capacities">Room Capacities</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/bookings">Bookings</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/prices">Room Prices</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/customers">Customers</a>
+                
+            </li>
+            <li class="nav-item list-group-item">
+                <a href="/customer/profile">Profile</a>
+                
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        
+        }
+    }
+</script>
