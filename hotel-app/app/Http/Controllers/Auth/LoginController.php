a2bab4de1ba1c7b791b19f19b069d71c0dd975bb
Eliasu Abraman <sabalisfi@yahoo.com>
2019-07-25 14:00:18 +0000

    redirect logged in user to bookings view

--- hotel-app/app/Http/Controllers/Auth/LoginController.php
+++ hotel-app/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/bookings';
 
     /**
      * Create a new controller instance.
